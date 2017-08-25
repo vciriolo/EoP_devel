8a62d1d855fec15d25e18b5a797bd1686b6e2408
Shervin <shervin@cern.ch>
2017-08-25 09:46:35 +0200

    script now provides the run ranges x2 tics

--- ZFitter/script/runLimitsTime.sh
+++ ZFitter/script/runLimitsTime.sh
@@ -1,21 +1,32 @@
 #!/bin/bash
-file=data/runRanges/runRangeLimits2.dat
+file=data/runRanges/monitoring_2017.dat
+echo "set x2tics (\\" > tmp/tics.label
+grep -v '#' $file | sed 's|-| |g' | awk '{printf("\"%d-%d\" %d 2 ,\\\n", $1, $2, ($4+$5)/2)}' >> tmp/tics.label
+echo ")" >> tmp/tics.label
+
+
+
+exit 0
+#
+file=data/runRanges/runRangeLimits.dat
 
 
 # output to a file with
 # run timeMin label
 
 # remove the commented lines
-sed '/^#/ d' $file > tmp/runLimit.clean
+sed '/^#/ d;' $file > tmp/runLimit.clean
 
 #find the timeMin
 for run in `cat tmp/runLimit.clean | sed 's| #.*||'`
 do
 	echo -ne $run"\t"
-	grep $run data/runRanges/monitoringRun2016.dat | sed 's|.*:||;s|#.*||' | awk '(NF==3){print $3}' | sed 's|-.*||' | sort | uniq
+	grep $run data/runRanges/monitoring_2017.dat 
+#| sed 's|.*:||;s|#.*||' | awk '(NF==3){print $3}' | sed 's|-.*||' | sort | uniq
 	echo 
-done | awk '(NF==2){print $0}' > tmp/runLimitTime
-
+done 
+#| awk '(NF==2){print $0}' > tmp/runLimitTime
+exit 0
 echo "set x2tics (\\" > $file.label
 for run in `cut -f 1 tmp/runLimitTime`
 do
