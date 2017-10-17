3b19b4b71eb49e4c9f08093fc742ee9f3b956fa5
Shervin <shervin@cern.ch>
2017-10-17 23:16:25 +0200

    re-reco with run-based pedestals + pulse shapes

---
+++ EcalAlCaRecoProducers/config/reRecoTags/Cal_Oct2017_PS_v1.py
@@ -0,0 +1,27 @@
+import FWCore.ParameterSet.Config as cms
+
+from CondCore.DBCommon.CondDBSetup_cfi import *
+
+RerecoGlobalTag = cms.ESSource("PoolDBESSource",
+                               CondDBSetup,
+                               connect = cms.string('frontier://FrontierProd/CMS_CONDITIONS'),
+                               globaltag = cms.string('92X_dataRun2_Prompt_v9'),
+                               toGet = cms.VPSet(
+        cms.PSet(record = cms.string("EcalPedestalsRcd"),
+                 tag = cms.string("EcalPedestals_Legacy2017_v1"),
+                 connect = cms.string("frontier://FrontierProd/CMS_CONDITIONS"),
+                 ),
+        cms.PSet(record = cms.string("EcalPulseShapesRcd"),
+                 tag = cms.string("EcalPulseShapes_October2017_rereco_v1_shervin"),
+                 connect = cms.string("frontier://FrontierPrep/CMS_CONDITIONS"),
+                 ),
+        cms.PSet(record = cms.string("EcalIntercalibConstantsRcd"),
+                 tag = cms.string("EcalIntercalibConstants_2017_2015_at_high_eta"),
+                 connect = cms.string("frontier://FrontierProd/CMS_CONDITIONS"),
+                 ),
+        cms.PSet(record = cms.string("EcalADCToGeVConstantRcd"),
+                 tag = cms.string("EcalADCToGeVConstant_plus_2.4prct_in_EE"),
+                 connect = cms.string("frontier://FrontierProd/CMS_CONDITIONS"),
+                 ),
+        ),
+                               )
