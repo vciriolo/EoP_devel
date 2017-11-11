1ed61afcbfbd6392f1e806be75c3dd164eb87253
Shervin <shervin@cern.ch>
2017-11-11 22:20:10 +0100

    removed last IOV from pulse shape tag for Oct2017 re-reco

---
+++ EcalAlCaRecoProducers/config/reRecoTags/Cal_Oct2017_cand_v3.py
@@ -0,0 +1,15 @@
+import FWCore.ParameterSet.Config as cms
+
+from CondCore.DBCommon.CondDBSetup_cfi import *
+
+RerecoGlobalTag = cms.ESSource("PoolDBESSource",
+                               CondDBSetup,
+                               connect = cms.string('frontier://FrontierProd/CMS_CONDITIONS'),
+                               globaltag = cms.string('92X_dataRun2_2017Repro_Candidate_2017_11_10_15_04_54'),
+                               toGet = cms.VPSet( 
+        cms.PSet(record = cms.string("EcalPulseShapesRcd"),
+                 tag = cms.string("EcalPulseShapes_October2017_rereco_v2"),
+                 connect = cms.string("frontier://FrontierProd/CMS_CONDITIONS"),
+                 ),
+        ),
+                               )
