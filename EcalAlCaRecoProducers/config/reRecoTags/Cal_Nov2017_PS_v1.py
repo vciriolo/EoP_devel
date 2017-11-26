c574dea1b7bc8930b72cd27bc3d144422c93a409
Shervin <shervin@cern.ch>
2017-11-26 07:38:09 +0100

    re-recoes introducting only 1 of the time-dependent tags of the Cal_Oct2017_cand_v4 tag

---
+++ EcalAlCaRecoProducers/config/reRecoTags/Cal_Nov2017_PS_v1.py
@@ -0,0 +1,24 @@
+import FWCore.ParameterSet.Config as cms
+# PS in RunE and RunF have the wrong IOV (bugged)
+from CondCore.DBCommon.CondDBSetup_cfi import *
+
+RerecoGlobalTag = cms.ESSource("PoolDBESSource",
+                               CondDBSetup,
+                               connect = cms.string('frontier://FrontierProd/CMS_CONDITIONS'),
+                               globaltag = cms.string('92X_dataRun2_2017Repro_Candidate_2017_11_10_15_04_54'),
+                               snapshotTime = cms.string("9999-12-31 23:59:59.000"),
+                               toGet = cms.VPSet(
+        # cms.PSet(record = cms.string("EcalPulseShapesRcd"),
+        #          tag = cms.string("EcalPulseShapes_1IOV_v1"),
+        #          connect = cms.string("frontier://FrontierPrep/CMS_CONDITIONS"),
+        #          ),
+        cms.PSet(record = cms.string("EcalPedestalsRcd"),
+                 tag = cms.string("EcalPedestals_1IOV_v1"),
+                 connect = cms.string("frontier://FrontierPrep/CMS_CONDITIONS"),
+                 ),
+        cms.PSet(record = cms.string("EcalIntercalibConstantsRcd"),
+                 tag = cms.string("EcalIntercalibConstants_1IOV_v1"),
+                 connect = cms.string("frontier://FrontierPrep/CMS_CONDITIONS"),
+                 ),
+        ),
+                               )
