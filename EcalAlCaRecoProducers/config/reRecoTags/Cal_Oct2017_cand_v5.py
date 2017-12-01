872fc30e3d4ae3f845df6c29b13c39f9def61145
Shervin <shervin@cern.ch>
2017-12-01 12:58:02 +0100

    fixed to run on 92X

--- EcalAlCaRecoProducers/config/reRecoTags/Cal_Oct2017_cand_v5.py
+++ EcalAlCaRecoProducers/config/reRecoTags/Cal_Oct2017_cand_v5.py
@@ -1,11 +1,39 @@
 import FWCore.ParameterSet.Config as cms
 
 from CondCore.DBCommon.CondDBSetup_cfi import *
-
+# this re-reco has the same tags as 
+# globaltag = cms.string('94X_dataRun2_Candidate_2017_12_01_11_09_37'),
+# but for 92X
 RerecoGlobalTag = cms.ESSource("PoolDBESSource",
                                CondDBSetup,
                                connect = cms.string('frontier://FrontierProd/CMS_CONDITIONS'),
-                               globaltag = cms.string('94X_dataRun2_Candidate_2017_12_01_11_09_37'),
+                               # globaltag = cms.string('94X_dataRun2_Candidate_2017_12_01_11_09_37'),
+                               globaltag = cms.string('92X_dataRun2_2017Repro_Candidate_2017_11_10_15_04_54'),
+                               snapshotTime = cms.string("9999-12-31 23:59:59.000"),
                                toGet = cms.VPSet( 
+        cms.PSet(record = cms.string("EcalPulseShapesRcd"),
+                 tag = cms.string("EcalPulseShapes_October2017_rereco_v3"),
+                 connect = cms.string("frontier://FrontierProd/CMS_CONDITIONS"),
+                 ),
+        cms.PSet(record = cms.string("EcalIntercalibConstantsRcd"),
+                 tag = cms.string("EcalIntercalibConstants_Run1_Run2_V04_offline_plusFixRunF"),
+                 connect = cms.string("frontier://FrontierProd/CMS_CONDITIONS"),
+                 ),
+        cms.PSet(record = cms.string("EcalLaserAPDPNRatiosRcd"),
+                 tag = cms.string("EcalLaserAPDPNRatios_offline_2016pp_legacy"),
+                 connect = cms.string("frontier://FrontierProd/CMS_CONDITIONS"),
+                 ),
+        cms.PSet(record = cms.string("EcalTimeCalibConstantsRcd"),
+                 tag = cms.string("EcalTimeCalibConstants_Run1_Run2_v02_offline"),
+                 connect = cms.string("frontier://FrontierProd/CMS_CONDITIONS"),
+                 ),
+        cms.PSet(record = cms.string("ESEEIntercalibConstantsRcd"),
+                 tag = cms.string("ESEEIntercalibConstants_V05_offline"),
+                 connect = cms.string("frontier://FrontierProd/CMS_CONDITIONS"),
+                 ),
+        cms.PSet(record = cms.string("ESIntercalibConstantsRcd"),
+                 tag = cms.string("ESIntercalibConstants_Run1_Run2_V11_offline"),
+                 connect = cms.string("frontier://FrontierProd/CMS_CONDITIONS"),
+                 ),
         ),
                                )
